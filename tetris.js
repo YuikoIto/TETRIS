44df197ac6af531d57664fe5602eaa01224760bb
YuikoIto <toffywing@gmail.com>
2020-08-18 23:59:12 +0900

    make move action

--- tetris.js
+++ tetris.js
@@ -6,9 +6,21 @@ var block_w = field_w / cols;
 var block_h = field_h / rows;
 var canvas = document.getElementById('field');
 var ctx = canvas.getContext('2d');
+var current_x = 0,
+  current_y = 0;
 
-ctx.strokeStyle = 'black';
+render();
+setInterval(tick, 1000);
 
-for (var i = 0; i < cols; i++) {
-  ctx.strokeRect(i * block_w, 0, block_w, block_h);
+function render() {
+  ctx.clearRect(0, 0, field_w, field_h);
+  ctx.strokeStyle = 'black';
+  for (var i = 0; i < cols; i++) {
+    ctx.strokeRect(i * block_w, current_y * block_h, block_w, block_h);
+  }
+}
+
+function tick() {
+  current_y++; // 下に移動させてから
+  render(); // 再描写
 }
