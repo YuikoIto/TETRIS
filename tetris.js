88b27bfccaf7cc5fad61c8988a3bf4aa71224f07
YuikoIto <toffywing@gmail.com>
2020-08-19 10:38:52 +0900

    change image

--- tetris.js
+++ tetris.js
@@ -6,17 +6,19 @@ var block_w = field_w / cols;
 var block_h = field_h / rows;
 var canvas = document.getElementById('field');
 var ctx = canvas.getContext('2d');
-var current_x = 0,
-  current_y = 0;
+var current_x = 0;
+var current_y = 0;
+const img = new Image();
+img.src = '121984.png';
 
 render();
 setInterval(tick, 1000);
 
 function render() {
   ctx.clearRect(0, 0, field_w, field_h);
-  ctx.strokeStyle = 'black';
+  img.onload = () => this.drawImage();
   for (var i = 0; i < cols; i++) {
-    ctx.strokeRect(i * block_w, current_y * block_h, block_w, block_h);
+    ctx.drawImage(img, i * block_w, current_y * block_h, block_w, block_h);
   }
 }
 
